091249230f6286a19579580e2e5f7ecf6015ade5
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-04-04 04:40:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+let array = ['anamta', 'musfirah', 2, 4, 8, "anas"];
+console.log(array[5]);
---
+++ app.ts
@@ -0,0 +1,2 @@
+let array: (string | number) [] = ['anamta','musfirah',2,4,8,"anas"];
+console.log(array[5])
